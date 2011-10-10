8f3b768d14f83ea799712bce4d34a23617ceaa5e
T6nis <kuuskass@gmail.com>
2011-10-10 14:18:02 +0300

    seni tehtud

---
+++ SIP Agent/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace SIP_Agent
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
