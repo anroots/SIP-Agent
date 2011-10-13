becfa3bd6b333eae2bde3f02f2cf7fdb794438ec
Sander Sylla <rednas2@gmail.com>
2011-10-13 22:58:18 +0300

    Esialgne UI uues branchis

---
+++ SIP-Agent/SIP Agent/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SIP_Agent
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void image1_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void scrollBar1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void UusTab_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Sip_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Salvesta_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+
+}
