d6d1ea9e836d141a943a3046452a067105bdcf83
unknown <david@.(none)>
2011-10-15 22:01:49 -0700

    Created a class for Task. Added some test controls to the main window and created a new window for standby mode.

--- SIP Agent/StandbyWindow.xaml.cs
+++ SIP Agent/StandbyWindow.xaml.cs
@@ -9,25 +9,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace SIP_Agent
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Standby.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Standby : Window
     {
-        public MainWindow()
+        public Standby()
         {
             InitializeComponent();
-        }
-
-        private void button2_Click(object sender, RoutedEventArgs e)
-        {
-            var Standby = new Standby();
-            Standby.Show();
         }
     }
 }
