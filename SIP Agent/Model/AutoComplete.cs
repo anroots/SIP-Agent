1353fcfa5f5d605eb6d907282e0422d3dbe44e93
Sander Sylla <rednas2@gmail.com>
2011-11-16 21:35:38 +0200

    Added person name query from db, autocomplete main structure, name field main logic, few bugs remain

---
+++ SIP Agent/Model/AutoComplete.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SIP_Agent.Model
+{
+    public class AutoComplete
+    {
+        //public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+
+
+        /// <summary>
+        /// Connect to database and select usable autocomplete values
+        /// </summary>
+        public static void ACNameValue()
+        {
+            using (DatabaseDataContext db = new DatabaseDataContext())
+            {
+                var query = (from x in db.persons
+                             select x).FirstOrDefault();
+                
+
+                string first_name = query.first_name;
+                string last_name = query.last_name;
+
+                List<string> PersonList = new List<string>();
+                PersonList.Add(first_name+ " " +last_name);
+
+                
+
+            }
+
+        }
+
+    }
+        /**
+    public class AutoCompletePhone
+    {
+            public int phone { get; set; }
+
+            public static void ACPhone(){
+
+            using (DatabaseDataContext db = new DatabaseDataContext()){
+
+          //  var query = (from x in db.persons
+                         
+                //TODO !! phone nr query
+          }
+
+        }
+
+    }
+  
+        * */
+}
+
