7dec7738c442c3b4089ef8fcfaf8c2d20790b59e
Sander Sylla <rednas2@gmail.com>
2011-11-18 18:35:00 +0200

    Added call information save to database. Additional info on issue tracker

---
+++ SIP Agent/Data/phone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SIP_Agent
+{
+    class phone
+    {
+        public int id { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public string person_id { get; set; }
+        public DateTime deleted { get; set; }
+
+    }
+}
