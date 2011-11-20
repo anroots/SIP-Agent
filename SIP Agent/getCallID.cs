268b3a0bdc2bf34fd2b8747e92dbc786abf91fc8
T6nis <kuuskass@gmail.com>
2011-11-20 20:52:03 +0200

    Another time.

---
+++ SIP Agent/getCallID.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SIP_Agent
+{
+    class QueryCallID
+    {
+        
+            public static void getCallID(int call)
+            {
+
+                using (DatabaseDataContext db = new DatabaseDataContext())
+                {
+                    var query = from x in db.calls
+                                where (x.id).Equals(call)
+                                select x;
+
+                    Console.WriteLine(query);               
+    
+            }
+               
+        }
+
+            public static void getCallLink(int call_id)
+            {
+                {
+                    
+
+                }
+
+            }
+    }
+}
