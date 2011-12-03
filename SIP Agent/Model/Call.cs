d3e0dfdb445d0937301dce0ff670539dd93c1850
Ando Roots <ando@roots.ee>
2011-12-03 11:03:34 -0800

    Deprecated Autocomplete* and getCallID, manually merged call playback from T6nis'es latest.

--- SIP Agent/Model/Call.cs
+++ SIP Agent/Model/Call.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Data.Linq.Mapping;
+using System.Xml.Linq;
+using System.Configuration;
 
 namespace SIP_Agent.Model
 {
@@ -50,7 +52,7 @@ namespace SIP_Agent.Model
                 finished = row.finished;
                 deleted = row.deleted;
                 Caller = new Person(caller_id);
-                
+
             }
         }
 
@@ -60,24 +62,26 @@ namespace SIP_Agent.Model
         /// <param name="db"></param>
         /// <param name="callID"></param>
         /// <returns></returns>
-        public call Get(DatabaseDataContext db, int callID = 0) {
+        public call Get(DatabaseDataContext db, int callID = 0)
+        {
 
-                var q = from x in db.calls where x.id.Equals(callID) && x.deleted.Equals(0) select x;
-                /*if (q.Count() == 0)
-                {
-                    throw new Exception("Call with ID "+ callID.ToString()+" not found");
-                }*/
+            var q = from x in db.calls where x.id.Equals(callID) && x.deleted.Equals(0) select x;
+            /*if (q.Count() == 0)
+            {
+                throw new Exception("Call with ID "+ callID.ToString()+" not found");
+            }*/
 
-                // Todo: refactor to bindings!
-                return q.FirstOrDefault();
-            }
-        
+            // Todo: refactor to bindings!
+            return q.FirstOrDefault();
+        }
+
 
         /// <summary>
         /// Save record back to the database
         /// </summary>
         /// <returns></returns>
-        public int Save() {
+        public int Save()
+        {
             using (DatabaseDataContext db = new DatabaseDataContext())
             {
                 if (id == 0)
@@ -87,11 +91,11 @@ namespace SIP_Agent.Model
                 }
                 var row = Get(db, id);
                 row.summary = summary;
-               try
+                try
                 {
                     db.SubmitChanges();
                     return id;
-               }
+                }
                 catch (Exception e)
                 {
                     return 0;
@@ -107,7 +111,8 @@ namespace SIP_Agent.Model
         {
             using (DatabaseDataContext db = new DatabaseDataContext())
             {
-                call current = new call { 
+                call current = new call
+                {
                     caller_id = caller_id,
                     clerk_id = App.CurrentUser.id,
                     received = DateTime.Now,
@@ -117,6 +122,55 @@ namespace SIP_Agent.Model
                 db.calls.InsertOnSubmit(current);
                 db.SubmitChanges();
                 return current.id;
+            }
+        }
+
+        /// <summary>
+        /// Get URL for the call .mp3 located in the calls server
+        /// </summary>
+        /// <param name="callId">The ID of the call</param>
+        /// <returns>URL to the file or null</returns>
+        public static string getCallLink(int callId)
+        {
+            // Contact the server API (XML response)
+            XDocument cl = XDocument.Load(ConfigurationManager.AppSettings["callApiURL"] + callId);
+
+            // Try to find the URL from the servers response
+            try
+            {
+                var calls = (from call in cl.Descendants("call")
+                             select call).Take(1);
+
+                if ((calls.Count()) < 1)
+                {
+                    return null;
+                }
+                return calls.FirstOrDefault().Element("url").Value;
+            }
+            // Return null on errors
+            catch (Exception e)
+            {
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Open the call sound file in an external (system default) sound player.
+        /// The file itself is streamed from a remote location
+        /// </summary>
+        /// <param name="callId">The ID of the call to play</param>
+        /// <returns>True on success, False on errors</returns>
+        public static bool PlayBack(int callId)
+        {
+            string callUrl = getCallLink(callId);
+            if (callUrl == null)
+            {
+                return false;
+            }
+            else
+            {
+                System.Diagnostics.Process.Start(callUrl);
+                return true;
             }
         }
     }
