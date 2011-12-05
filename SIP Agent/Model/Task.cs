972881391db90f3ebc60df1356b84d6a0f0fed2c
Ando Roots <ando@roots.ee>
2011-12-05 15:06:53 -0800

    Task view: first bindings operational

--- SIP Agent/Model/Task.cs
+++ SIP Agent/Model/Task.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.Linq.Mapping;
 
 namespace SIP_Agent.Model
 {
+    [Table(Name = "Calls")]
     public class Task
     {
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)]
         public int id { get; set; }
         public int parent_id { get; set; }
         public DateTime created { get; set; }
@@ -14,10 +17,36 @@ namespace SIP_Agent.Model
         public string title { get; set; }
         public string details { get; set; }
         public int notifier_id { get; set; }
-        public int assignee_id { get; set; }
+        public int? assignee_id { get; set; }
         public int clerk_id { get; set; }
         public int status_id { get; set; }
         public int category_id { get; set; }
         public int deleted { get; set; }
+
+        public Task(int TaskId)
+        {
+            using (DatabaseDataContext db = new DatabaseDataContext())
+            {
+                var row = Get(db, TaskId);
+
+                id = row.id;
+                title = row.title;
+                status_id = row.status_id;
+                assignee_id = row.assignee_id;
+            }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="db"></param>
+        /// <param name="TaskId"></param>
+        /// <returns></returns>
+        public task Get(DatabaseDataContext db, int TaskId = 0)
+        {
+            var q = from x in db.tasks where x.id.Equals(TaskId) && x.deleted.Equals(0) select x;
+            return q.FirstOrDefault();
+        }
     }
 }
--- SIP Agent/View/TaskView.xaml.cs
+++ SIP Agent/View/TaskView.xaml.cs
@@ -19,18 +19,56 @@ namespace SIP_Agent
     /// </summary>
     public partial class TaskView : UserControl, ISwitchable
     {
+        public Model.Task CurrentTask;
 
         public TaskView(int TaskId)
         {
             InitializeComponent();
 
+            // Load task info
+            CurrentTask = new Model.Task(TaskId);
+
+            // Populate controls with info
+            LoadValues();
+
         }
 
+        /// <summary>
+        /// Load control values
+        /// </summary>
+        private void LoadValues()
+        {
+
+            txtTitle.DataContext = CurrentTask; // Title
+
+            // Select data sources from DB
+            using (DatabaseDataContext db = new DatabaseDataContext())
+            {
+                // Status dropdown
+                var statuses = from row in db.task_statuses
+                               select new { id = row.id, name = row.name.Trim() };
+
+                cmbStatus.ItemsSource = statuses;
+                cmbStatus.DisplayMemberPath = "name";
+                cmbStatus.SelectedValuePath = "id";
+                cmbStatus.SelectedIndex = CurrentTask.status_id;
+
+                // Persons dropdown
+                var persons = from row in db.persons
+                              select new { id = row.id, name = row.first_name.Trim() + " "+row.last_name.Trim() };
+
+                cmbAssignee.ItemsSource = persons;
+                cmbAssignee.DisplayMemberPath = "name";
+                cmbAssignee.SelectedValuePath = "id";
+
+                cmbAssignee.SelectedIndex = CurrentTask.assignee_id != null ? (int)CurrentTask.assignee_id : 0;
+            }
+        }
 
         #region Event For Child Window
         private void btn_login_Click(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            
+
         }
 
 
@@ -48,10 +86,30 @@ namespace SIP_Agent
 
         #endregion
 
-      
+        /// <summary>
+        /// Move back to the Standby screen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new View.SystemStandby());
+        }
 
-       
+        /// <summary>
+        /// Save task info
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            // todo!
+        }
 
-      
+
+
+
+
+
     }
 }
